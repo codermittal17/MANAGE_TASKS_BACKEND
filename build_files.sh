c02efb9ac8a1cc53081060764c13babe3099cd62
Raghvendra <codermittal17@gmail.com>
2023-07-31 22:29:46 +0530

    changed static

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,6 @@
 echo " BUILD START"
-pip install -r requirements.txt
-python3.10 manage.py collectstatic --noinput --clear
+python3.9 -m pip3 install -r requirements.txt
+python3.9 manage.py collectstatic --noinput --clear
 echo " BUILD END"
 
 
